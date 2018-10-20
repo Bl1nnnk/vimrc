54bf341a33d4dd7b9eabd9f46269468e988e894d
JunGu <da2din9o@wd.lan>
2018-10-20 14:24:52 +0000

    fixed bug of downloading ack

--- install_basic_vimrc.sh
+++ install_basic_vimrc.sh
@@ -9,6 +9,6 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 if [ $? -eq 0 ]; then
     git clone https://github.com/mileszs/ack.vim.git ~/.vim/bundle/ack.vim
     git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
-    sudo curl bash -c "https://beyondgrep.com/ack-2.18-single-file > /bin/ack && chmod 0755 /bin/ack"
+    sudo bash -c "curl https://beyondgrep.com/ack-2.18-single-file > /bin/ack && chmod 0755 /bin/ack"
 fi
 echo "Installed the Basic Vim configuration successfully! Enjoy :-)"

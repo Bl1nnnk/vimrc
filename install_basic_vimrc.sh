655887b91147d830c29d5ef9972c9bc84ccaeaa9
jungu <dust4151@gmail.com>
2020-09-11 01:13:51 +0800

    bash banner

--- install_basic_vimrc.sh
+++ install_basic_vimrc.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #cd ~/.vim_runtime
 #cat ~/.vim_runtime/vimrcs/basic.vim > ~/.vimrc
 cat ./vimrcs/basic.vim > ~/.vimrc
@@ -10,6 +12,8 @@ if [ $? -eq 0 ]; then
     git clone https://github.com/mileszs/ack.vim.git ~/.vim/bundle/ack.vim
     git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
     sudo bash -c "curl https://beyondgrep.com/ack-2.18-single-file > /bin/ack && chmod 0755 /bin/ack"
+else
+	exit
 fi
 
 if [ $? -eq 0 ]; then
